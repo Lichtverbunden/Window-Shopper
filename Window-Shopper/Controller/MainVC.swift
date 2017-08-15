14937fd95c8e0a0624cf545dceac9c275e04d6ef
Ken Krippeler <kriken19@gmail.com>
2017-08-15 22:24:07 +0200

    First Setup

---
+++ Window-Shopper/Controller/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  MainVC.swift
+//  Window-Shopper
+//
+//  Created by Ken Krippeler on 15.08.17.
+//  Copyright © 2017 Lichtverbunden. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
